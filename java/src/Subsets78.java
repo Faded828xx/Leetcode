224b157d6c704fbaa923c669b35cd46694869fd5
Faded828xx <zhengyouheng732@gmail.com>
2021-04-01 11:00:18 +0800

    数组的所有子集

---
+++ java/src/Subsets78.java
@@ -0,0 +1,73 @@
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。 
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0]
+//输出：[[],[0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10 
+// -10 <= nums[i] <= 10 
+// nums 中的所有元素 互不相同 
+// 
+// Related Topics 位运算 数组 回溯算法 
+// 👍 1103 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Subsets78 {
+
+    List<Integer> cur = new ArrayList<>();  // o~index 的状态
+    List<List<Integer>> res = new ArrayList<>();    // 目标数组
+
+    // 回溯算法
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(nums,0);
+        return res;
+    }
+    public void dfs(int[] nums, int index) {    // 确定0～index的状态
+        if(index==nums.length) {    // 当前状态结束
+            res.add(new ArrayList<>(cur));
+            return ;
+        }
+        cur.add(nums[index]);
+        dfs(nums,index+1);
+        cur.remove(cur.size()-1);
+        dfs(nums,index+1);
+    }
+
+    // 迭代算法
+    public List<List<Integer>> subSets(int[] nums) {
+        int len = nums.length;
+        for(int i=0; i<(1<<len); i++) { // 用二进制码表示nums元素的选中状态
+            List<Integer> cur = new ArrayList<>();
+            for(int j=0; j<len; j++) {
+                if((i&(1<<j))!=0) {
+                    cur.add(nums[j]);
+                }
+            }
+            res.add(cur);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ java/src/SubsetsIi90.java
@@ -0,0 +1,65 @@
+//给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。 
+//
+// 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。 
+//
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,2]
+//输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0]
+//输出：[[],[0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10 
+// -10 <= nums[i] <= 10 
+// 
+// 
+// 
+// Related Topics 数组 回溯算法 
+// 👍 523 👎 0
+
+
+import java.util.ArrayList;
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SubsetsIi90 {
+
+    Set<List<Integer>> res = new HashSet<>();
+
+    // 迭代
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int len = nums.length;
+        for(int i=0; i<(1<<len); i++) { // 用二进制码表示nums元素的选中状态
+            List<Integer> cur = new ArrayList<>();
+            for(int j=0; j<len; j++) {
+                if((i&(1<<j))!=0) {
+                    cur.add(nums[j]);
+                }
+            }
+            Collections.sort(cur);  // 要先排序才添加到集合中
+            res.add(cur);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for(List<Integer> list : res) {
+            result.add(list);
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
