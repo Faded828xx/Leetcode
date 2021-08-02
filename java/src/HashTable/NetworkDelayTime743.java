7e06f06579c501de9217841ee6c6a064a5e4531e
Faded828xx <zhengyouheng732@gmail.com>
2021-08-02 09:39:40 +0800

    网络延迟时间

---
+++ java/src/HashTable/NetworkDelayTime743.java
@@ -0,0 +1,103 @@
+package HashTable;
+//有 n 个网络节点，标记为 1 到 n。
+//
+// 给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， w
+//i 是一个信号从源节点传递到目标节点的时间。 
+//
+// 现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回 -1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：times = [[1,2,1]], n = 2, k = 1
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：times = [[1,2,1]], n = 2, k = 2
+//输出：-1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= n <= 100 
+// 1 <= times.length <= 6000 
+// times[i].length == 3 
+// 1 <= ui, vi <= n 
+// ui != vi 
+// 0 <= wi <= 100 
+// 所有 (ui, vi) 对都 互不相同（即，不含重复边） 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 图 最短路 堆（优先队列） 
+// 👍 296 👎 0
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class NetworkDelayTime743 {
+
+    // 没AC
+    public static int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        for (int[] time : times) {
+            int from = time[0];
+            List<int[]> list = map.getOrDefault(from, new ArrayList<>());
+            list.add(time);
+            map.put(from, list);
+        }
+        boolean[] visited = new boolean[n + 1];
+        // 到达节点i用时resTime[i]
+        int[] resTime = new int[n + 1];
+        Arrays.fill(resTime, Integer.MAX_VALUE);
+        resTime[k] = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(k);
+        while (!queue.isEmpty()) {
+            int cur = queue.remove();
+            visited[cur] = true;
+            List<int[]> list = map.get(cur);
+            if (list == null) continue;
+            for (int[] next : list) {
+                int nextNode = next[1];
+                int nextTime = next[2];
+                resTime[nextNode] = Math.min(resTime[nextNode], resTime[cur] + nextTime);
+                if (!visited[nextNode]) {
+                    queue.add(nextNode);
+                }
+//                int a = resTime[nextNode];
+//                int b = resTime[cur] + nextTime;
+//                if (a == Integer.MAX_VALUE || b <= a) {
+//                    resTime[nextNode] = b;
+//                    queue.add(nextNode);
+//                }
+            }
+        }
+        int res = 0;
+        for (int i = 1; i < n + 1; i++)
+            if (resTime[i] == Integer.MAX_VALUE && i != k) return -1;
+            else res = Math.max(res, resTime[i]);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] times = new int[][]{{2, 3, 7}, {1, 2, 1}, {1, 3, 4}, {2, 1, 2}};
+        System.out.println(networkDelayTime(times, 3, 2));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
