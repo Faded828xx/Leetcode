99126e970f672be74852c2fba946e8aa97e86d36
Faded828x <zhengyouheng732@gmail.com>
2021-09-11 22:09:00 +0800

    心算挑战

--- java/src/Contest/Other/LeetcodeCup.java
+++ java/src/Contest/Other/LeetcodeCup.java
@@ -38,6 +38,7 @@ public class LeetcodeCup {
     }
 
 
+    // 比赛的时候一直挂 赛后看到错误用例 一会就调出来了 好菜啊
     public static int maxmiumScore(int[] cards, int cnt) {
         int[][] dp = new int[cnt + 1][2];
         for(int[] d : dp)
@@ -52,51 +53,32 @@ public class LeetcodeCup {
             cards2[i] = cards[i];
         Arrays.sort(cards2, (a1, a2) -> a2 - a1);
 
-//        System.out.println(Arrays.toString(cards2));
-
-
         int limit1 = 1;
         int limit2 = 1;
         for(int i = 0; i < len; i++) {
             int card = cards2[i];
             boolean isOdd = card % 2 == 1;
-//            int j = Math.min(i + 1, cnt);
-//            for(; j >= 1; j--) {
-//                if(isOdd) {
-//                    if(dp[j - 1][1] != -1)
-//                        dp[j][0] = Math.max(dp[j][0], dp[j - 1][1] + card);
-//                    if(dp[j - 1][0] != -1 && j != 1)
-//                        dp[j][1] = Math.max(dp[j][1], dp[j - 1][0] + card);
-//                } else {
-//                    if(dp[j - 1][1] != -1)
-//                        dp[j][1] = Math.max(dp[j][1], dp[j - 1][1] + card);
-//                    if(dp[j - 1][0] != -1 && j != 1)
-//                        dp[j][0] = Math.max(dp[j][0], dp[j - 1][0] + card);
-//                }
-//            }
-
-
-            if(isOdd) {
-                if(dp[limit1 - 1][1] != -1) {
-                    dp[limit1][0] = Math.max(dp[limit1][0], dp[limit1 - 1][1] + card);
-                    if(limit1 != cnt)
-                        limit1++;
-                }
-                if(dp[limit2 - 1][0] != -1 && limit2 != 1) {
-                    dp[limit2][1] = Math.max(dp[limit2][1], dp[limit2 - 1][0] + card);
-                    if(limit2 != cnt)
-                        limit2++;
-                }
-            } else {
-                if(dp[limit1 - 1][0] != -1 && limit1 != 1) {
-                    dp[limit1][0] = Math.max(dp[limit1][0], dp[limit1 - 1][0] + card);
-                    if(limit1 != cnt)
-                        limit1++;
-                }
-                if(dp[limit2 - 1][1] != -1) {
-                    dp[limit2][1] = Math.max(dp[limit2][1], dp[limit2 - 1][1] + card);
-                    if(limit2 != cnt)
-                        limit2++;
+            int j = Math.min(i + 1, cnt);
+            int limit = Math.min(limit1, limit2);
+            for(; j >= limit; j--) {
+                if(isOdd) {
+                    if(dp[j - 1][1] != -1) {
+                        dp[j][0] = Math.max(dp[j][0], dp[j - 1][1] + card);
+                        limit1 = Math.max(limit1, j);
+                    }
+                    if(dp[j - 1][0] != -1 && j != 1) {
+                        dp[j][1] = Math.max(dp[j][1], dp[j - 1][0] + card);
+                        limit2 = Math.max(limit2, j);
+                    }
+                } else {
+                    if(dp[j - 1][1] != -1) {
+                        dp[j][1] = Math.max(dp[j][1], dp[j - 1][1] + card);
+                        limit2 = Math.max(limit2, j);
+                    }
+                    if(dp[j - 1][0] != -1 && j != 1) {
+                        dp[j][0] = Math.max(dp[j][0], dp[j - 1][0] + card);
+                        limit1 = Math.max(limit1, j);
+                    }
                 }
             }
 
@@ -107,13 +89,55 @@ public class LeetcodeCup {
 //            if(limit2 == cnt + 1 && limit1 == cnt + 1)
 //                return dp[cnt][1];
         }
+
+        if(limit1 == cnt && limit2 == cnt && dp[cnt][0] != -1 && dp[cnt][1] != -1)
+            return dp[cnt][1];
         return dp[cnt][1] == -1 ? 0 : dp[cnt][1];
     }
 
+    // 上一题贪心解法
+    public int maxmiumScore2(int[] cards, int cnt) {
+        int len = cards.length;
+        Integer[] cards2 = new Integer[len];
+        for(int i = 0; i < len; i++)
+            cards2[i] = cards[i];
+        Arrays.sort(cards2, (a1, a2) -> a2 - a1);
+
+        int res = 0;
+        for(int i = 0; i < cnt; i++)
+            res += cards2[i];
+        if(res % 2 == 0) return res;
+        int preOdd = 0, preEven = 0, postOdd = 0, postEven = 0;
+        for(int i = cnt - 1; i >= 0; i--) {
+            if(preOdd == 0 && cards2[i] % 2 == 1)
+                preOdd = cards2[i];
+            if(preEven == 0 && cards2[i] % 2 == 0)
+                preEven = cards2[i];
+            if(preOdd != 0 && preEven != 0)
+                break;
+        }
+        for(int i = cnt; i < len; i++) {
+            if(postOdd == 0 && cards2[i] % 2 == 1)
+                postOdd = cards2[i];
+            if(postEven == 0 && cards2[i] % 2 == 0)
+                postEven = cards2[i];
+            if(postOdd != 0 && postEven != 0)
+                break;
+        }
+        int diff1 = (preOdd != 0 && postEven != 0) ? preOdd - postEven : Integer.MAX_VALUE;;
+        int diff2 = (preEven != 0 && postOdd != 0) ? preEven - postOdd : Integer.MAX_VALUE;
+        if(diff1 == Integer.MAX_VALUE && diff2 == Integer.MAX_VALUE)
+            return 0;
+        return res - Math.min(diff1, diff2);
+    }
+
+
+
+
 
     public static void main(String[] args) {
-        int[] cards = new int[]{1,2,8,9};
-        System.out.println(maxmiumScore(cards, 3));
+        int[] cards = new int[]{1,10,5,2,9};
+        System.out.println(maxmiumScore(cards, 4));
     }
 
 }
