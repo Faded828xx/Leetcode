df4f490532cebbe082762435496d09757aa98c86
Faded828x <zhengyouheng732@gmail.com>
2021-10-28 09:06:13 +0800

    重新排序得到2的幂

--- java/src/Math/ReorderedPowerOf2869.java
+++ java/src/Math/ReorderedPowerOf2869.java
@@ -1,4 +1,5 @@
-package Math;//You are given an integer n. We reorder the digits in any order (including the
+package Math;
+//You are given an integer n. We reorder the digits in any order (including the
 //original order) such that the leading digit is not zero. 
 //
 // Return true if and only if we can do this so that the resulting number is a p
@@ -55,6 +56,7 @@ import java.util.Arrays;
 //leetcode submit region begin(Prohibit modification and deletion)
 class ReorderedPowerOf2869 {
     public boolean reorderedPowerOf2(int n) {
+        // 这里直接打表了 当然也是程序跑出来的
         int[] pow = new int[]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
                 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,
                 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728,
@@ -70,7 +72,5 @@ class ReorderedPowerOf2869 {
         return false;
     }
 
-    public static void main(String[] args) {
-    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
